def12252b2230aba76e590512a6ff566ea51a23e
Samer Sallam <samersallam92@gmail.com>
2022-05-23 10:54:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title("My Parents New Healthy Dinner")
 
 streamlit.header('Breakfast Menu')
@@ -7,3 +9,6 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
